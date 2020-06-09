35d7d41c5f2ebf9b8293cb2fc1b4b9a36948bb12
Daniel <danilu01@ucm.es>
2020-06-09 15:41:40 +0200

    Vista abrir venta

--- src/presentacion/view/ventas/AbrirVenta.java
+++ src/presentacion/view/ventas/AbrirVenta.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.ScrollPane;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JList;
@@ -57,23 +59,25 @@ public class AbrirVenta implements ActionListener{
 		selectedProductsModel = new DefaultListModel<String>();
 		productsModel = new DefaultListModel<String>();
 		
-		buttonSelect = new JButton(">>");
+		buttonSelect = new JButton("Agregar producto");
 		buttonSelect.addActionListener(this);
-		buttonUnselect = new JButton("<<");
+		buttonUnselect = new JButton("Eliminar producto");
 		buttonUnselect.addActionListener(this);
 		
 		for(int i = 0; i < productos.size(); i++) productsModel.addElement(productos.get(i).getNombre());
 		productsList = new JList<String>(productsModel);
 		productsList.setVisibleRowCount(10);
-		productsList.setFixedCellHeight(20);
-		productsList.setFixedCellWidth(140);
-		productsList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);	
+		productsList.setFixedCellHeight(25);
+		productsList.setFixedCellWidth(200);
+		productsList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		productsList.setFont(new Font(productsList.getFont().toString(), Font.PLAIN, 15));
 
 		selectedProductsList = new JList<String>(selectedProductsModel);
 		selectedProductsList.setVisibleRowCount(10);
-		selectedProductsList.setFixedCellHeight(20);
-		selectedProductsList.setFixedCellWidth(140);
+		selectedProductsList.setFixedCellHeight(25);
+		selectedProductsList.setFixedCellWidth(200);
 		selectedProductsList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		selectedProductsList.setFont(new Font(selectedProductsList.getFont().toString(), Font.PLAIN, 15));
 		
 		confirmar.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e ) {  }
@@ -89,38 +93,69 @@ public class AbrirVenta implements ActionListener{
 		JPanel abrirVentaPanel= new JPanel(new BorderLayout());
 		
 		JScrollPane list1 = new JScrollPane(productsList);
-		JScrollPane list2 = new JScrollPane(selectedProductsList); 
+		list1.setPreferredSize(new Dimension(200,350));
+		JScrollPane list2 = new JScrollPane(selectedProductsList);
+		list2.setPreferredSize(new Dimension(200,350));
 		
 		JPanel campos = new JPanel(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		
 		c.gridx = 0;
 		c.gridy = 0;
-		campos.add(SwingFactory.getJLabel(new Dimension(200,50), "                DNI CLIENTE:" ,30), c);
+		campos.add(SwingFactory.getJLabel(new Dimension(200,50), "ID CLIENTE:" ,30), c);
 		c.gridx = 1;
 		c.gridy = 0;
 		campos.add(clienteTF, c);
 		c.gridx = 0;
 		c.gridy = 1;
-		campos.add(SwingFactory.getJLabel(new Dimension(200,50), "                DNI EMPLEADO:" ,30), c);
+		campos.add(SwingFactory.getJLabel(new Dimension(250,50), "ID EMPLEADO:" ,30), c);
 		c.gridx = 1;
 		c.gridy = 1;
 		campos.add(personalTF, c);
 		c.gridx = 0;
 		c.gridy = 2;
-		campos.add(SwingFactory.getJLabel(new Dimension(200,50), "               PRODUCTOS:" ,30), c);
+		c.gridwidth = 2;
+		c.gridheight = 1;
+		campos.add(SwingFactory.getJLabel(new Dimension(220,80), "PRODUCTOS:" ,30), c);
+		
+		JPanel listas = new JPanel(new GridBagLayout());
+		GridBagConstraints contraintsListas = new GridBagConstraints();
+		
+		contraintsListas.gridx = 0;
+		contraintsListas.gridy = 1;
+		listas.add(SwingFactory.getJLabel(new Dimension(170,30), " Productos disponibles" ,15), contraintsListas);
+		contraintsListas.gridx = 1;
+		contraintsListas.gridy = 1;
+		listas.add(Box.createRigidArea(new Dimension(150, 0)), contraintsListas);
+		contraintsListas.gridx = 2;
+		contraintsListas.gridy = 1;
+		listas.add(SwingFactory.getJLabel(new Dimension(160,30), "  Productos a vender" ,15), contraintsListas);
+		contraintsListas.gridx = 0;
+		contraintsListas.gridy = 2;
+		listas.add(list1, contraintsListas);
+		contraintsListas.gridx = 2;
+		contraintsListas.gridy = 2;
+		listas.add(list2, contraintsListas);
+		contraintsListas.gridx = 0;
+		contraintsListas.gridy = 3;
+		contraintsListas.gridwidth = 2;
+		contraintsListas.gridheight = 1;
+		listas.add(Box.createRigidArea(new Dimension(0, 10)), contraintsListas);
+		contraintsListas.gridx = 0;
+		contraintsListas.gridy = 4;
+		contraintsListas.gridwidth = 1;
+		contraintsListas.gridheight = 1;
+		listas.add(buttonSelect, contraintsListas);
+		contraintsListas.gridx = 2;
+		contraintsListas.gridy = 4;
+		listas.add(buttonUnselect, contraintsListas);
+		
 		c.gridx = 0;
 		c.gridy = 3;
-		campos.add(buttonSelect, c);
-		c.gridx = 1;
-		c.gridy = 3;
-		campos.add(buttonUnselect, c);
-		c.gridx = 0;
-		c.gridy = 4;
-		campos.add(list1, c);
-		c.gridx = 1;
-		c.gridy = 4;
-		campos.add(list2, c);
+		c.gridwidth = 2;
+		c.gridheight = 1;
+		campos.add(listas, c);
+		
 		
 		JPanel botones = new JPanel();
 		botones.setLayout(new FlowLayout( FlowLayout.CENTER ));
@@ -160,23 +195,25 @@ public class AbrirVenta implements ActionListener{
 		selectedProductsModel = new DefaultListModel<String>();
 		productsModel = new DefaultListModel<String>();
 		
-		buttonSelect = new JButton(">>");
+		buttonSelect = new JButton("Agregar producto");
 		buttonSelect.addActionListener(this);
-		buttonUnselect = new JButton("<<");
+		buttonUnselect = new JButton("Eliminar producto");
 		buttonUnselect.addActionListener(this);
 		
 		for(int i = 0; i < productos.size(); i++) productsModel.addElement(productos.get(i).getNombre());
 		productsList = new JList<String>(productsModel);
 		productsList.setVisibleRowCount(10);
-		productsList.setFixedCellHeight(20);
+		productsList.setFixedCellHeight(25);
 		productsList.setFixedCellWidth(140);
-		productsList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);	
+		productsList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		productsList.setFont(new Font(productsList.getFont().toString(), Font.PLAIN, 15));
 
 		selectedProductsList = new JList<String>(selectedProductsModel);
 		selectedProductsList.setVisibleRowCount(10);
-		selectedProductsList.setFixedCellHeight(20);
+		selectedProductsList.setFixedCellHeight(25);
 		selectedProductsList.setFixedCellWidth(140);
 		selectedProductsList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		selectedProductsList.setFont(new Font(selectedProductsList.getFont().toString(), Font.PLAIN, 15));
 	}
 	
 	private void limpiar() {
